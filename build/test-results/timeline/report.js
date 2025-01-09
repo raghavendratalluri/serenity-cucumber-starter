43b4616e922241a333c47062109ba1326f85b8e8
Talluri <venkata.talluri@capgemini.com>
2025-01-08 21:22:38 -0600

    second commit

--- build/test-results/timeline/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"53a39f59-ee09-4f39-88bc-c4f9f89f8b17","feature":"Create Customer","scenario":"Create New Customer by filling all fields","start":1736341123318,"group":1,"content":"","tags":"@createnewcustomer,","end":1736341136676,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
